f04dfb4253ec868af16f1cc8e91eef767ad74961
Allen Chang <allenchang616@gmail.com>
2018-05-15 22:47:30 +0800

    update program banner

--- tools/FASTQSplitter.py
+++ tools/FASTQSplitter.py
@@ -1,20 +1,9 @@
 #
 # @note Copyright (C) 2018, Atgenomix Incorporated. All Rights Reserved.
-#       This program is an unpublished copyrighted work which is proprietary to
-#       Atgenomix Incorporated and contains confidential information that is not to
-#       be reproduced or disclosed to any other person or entity without prior
-#       written consent from Atgenomix, Inc. in each and every instance.
+#       This program is a copyrighted work which is proprietary to
+#       Atgenomix Incorporated and released under GNU GPLv3.
 #
-# @warning Unauthorized reproduction of this program as well as unauthorized
-#          preparation of derivative works based upon the program or distribution of
-#          copies by sale, rental, lease or lending are violations of federal copyright
-#          laws and state trade secret laws, punishable by civil and criminal penalties.
-#
-# @file    tools/FASTQSplitter.py
-#
-# @brief   Get View results
-#
-# @author  Yun-Lung Li (liyunlung@gmail.com)
+# @author  Yun-Lung Li (yunlung_li@atgenomix.com)
 #
 # @date    2018/03/23
 #
